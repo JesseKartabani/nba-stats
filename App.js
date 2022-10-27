29bec48dbd34860fe9acb84dcb71cf5daad748fd
Jesse1224 <jessekartabani@gmail.com>
2022-10-27 14:33:18 +1100

    Boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import LeagueLeaders from "./components/LeagueLeaders";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <LeagueLeaders />
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/LeagueLeaders.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const LeagueLeaders = () => {
+  return (
+    <SafeAreaView>
+      <Text>LeagueLeaders</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LeagueLeaders;
+
+const styles = StyleSheet.create({});

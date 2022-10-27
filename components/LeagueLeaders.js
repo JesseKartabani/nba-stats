ccbacd5234170a530174647302e51b0f0ec5e10c
Jesse1224 <jessekartabani@gmail.com>
2022-10-27 15:28:00 +1100

    fetch data from nba api

--- components/LeagueLeaders.js
+++ components/LeagueLeaders.js
@@ -1,10 +1,30 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const LeagueLeaders = () => {
+const LeagueLeaders = ({ rank }) => {
+  const [stats, setStats] = useState([]);
+  // fetch data
+  const getLeagueLeaders = async () => {
+    try {
+      const response = await fetch(
+        "https://stats.nba.com/stats/leagueleaders?ActiveFlag=&LeagueID=00&PerMode=Totals&Scope=S&Season=2020-21&SeasonType=Regular+Season&StatCategory=PTS"
+      );
+      const json = await response.json();
+      const data = json.resultSet.rowSet[rank];
+      console.log(data);
+      setStats(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getLeagueLeaders();
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>LeagueLeaders</Text>
+      <Text>{stats[2]}</Text>
     </SafeAreaView>
   );
 };

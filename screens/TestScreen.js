020a65fbf24bd9fd590d67b8b0c02c543e5c7cb2
Jesse1224 <jessekartabani@gmail.com>
2022-10-28 19:18:08 +1100

    boilerplate for test screen

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,16 +1,31 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ImageBackground,
+} from "react-native";
 import React from "react";
 import NavOptions from "../components/NavOptions";
+import Banner from "../components/Banner";
 
 const TestScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>TestScreen</Text>
+    <ImageBackground
+      style={styles.backgroundImg}
+      source={{ uri: "https://i.imgur.com/Fw7YF2Q.jpg" }}
+    >
+      <Banner />
       <NavOptions />
-    </SafeAreaView>
+    </ImageBackground>
   );
 };
 
 export default TestScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundImg: {
+    width: "100%",
+    height: "100%",
+  },
+});

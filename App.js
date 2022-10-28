3c47ca66c20d9c941e1a1640304df2fa9e4e9275
Jesse1224 <jessekartabani@gmail.com>
2022-10-28 15:23:17 +1100

    add background img for whole app

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, ScrollView, StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Banner from "./components/Banner";
 import LeagueLeaders from "./components/LeagueLeaders";
@@ -7,23 +6,31 @@ import LeagueLeaders from "./components/LeagueLeaders";
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
-      <Banner />
-      <ScrollView bounces={false}>
-        <LeagueLeaders rank="0" />
-        <LeagueLeaders rank="1" />
-        <LeagueLeaders rank="2" />
-        <LeagueLeaders rank="3" />
-        <LeagueLeaders rank="4" />
-        <LeagueLeaders rank="5" />
-        <LeagueLeaders rank="6" />
-        <LeagueLeaders rank="7" />
-        <LeagueLeaders rank="8" />
-        <LeagueLeaders rank="9" />
-      </ScrollView>
+      <ImageBackground
+        style={styles.backgroundImg}
+        source={{ uri: "https://i.imgur.com/Fw7YF2Q.jpg" }}
+      >
+        <Banner />
+        <ScrollView bounces={false}>
+          <LeagueLeaders rank="0" />
+          <LeagueLeaders rank="1" />
+          <LeagueLeaders rank="2" />
+          <LeagueLeaders rank="3" />
+          <LeagueLeaders rank="4" />
+          <LeagueLeaders rank="5" />
+          <LeagueLeaders rank="6" />
+          <LeagueLeaders rank="7" />
+          <LeagueLeaders rank="8" />
+          <LeagueLeaders rank="9" />
+        </ScrollView>
+      </ImageBackground>
     </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  backgroundImg: {
+    height: "100%",
+    width: "100%",
+  },
 });

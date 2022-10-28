37f7820a3a9e76fa04a11ae1b1a9a974bdd8d649
Jesse1224 <jessekartabani@gmail.com>
2022-10-28 20:23:42 +1100

    Fetch player career stats (Broken)

--- components/PlayerCareerStats.js
+++ components/PlayerCareerStats.js
@@ -1,7 +1,29 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { React, useState, useEffect } from "react";
 
 const PlayerCareerStats = () => {
+  {
+    /* Endpoint isnt working */
+  }
+  const [stats, setStats] = useState([]);
+  const getPlayerCareerStats = async () => {
+    try {
+      const response = await fetch(
+        "https://stats.nba.com/stats/playercareerstats?LeagueID=&PerMode=Totals&PlayerID=2544"
+      );
+      const json = await response.json();
+      const data = json;
+      console.log(data);
+      setStats(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPlayerCareerStats();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>Test</Text>

a0062e833a400afe5e694fc9b9924907ae826087
Jesse1224 <jessekartabani@gmail.com>
2022-10-28 14:54:55 +1100

    Vertical scroll league leaders(for small screens)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Banner from "./components/Banner";
 import LeagueLeaders from "./components/LeagueLeaders";
@@ -8,25 +8,22 @@ export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
       <Banner />
-      <LeagueLeaders rank="0" />
-      <LeagueLeaders rank="1" />
-      <LeagueLeaders rank="2" />
-      <LeagueLeaders rank="3" />
-      <LeagueLeaders rank="4" />
-      <LeagueLeaders rank="5" />
-      <LeagueLeaders rank="6" />
-      <LeagueLeaders rank="7" />
-      <LeagueLeaders rank="8" />
-      <LeagueLeaders rank="9" />
+      <ScrollView bounces={false}>
+        <LeagueLeaders rank="0" />
+        <LeagueLeaders rank="1" />
+        <LeagueLeaders rank="2" />
+        <LeagueLeaders rank="3" />
+        <LeagueLeaders rank="4" />
+        <LeagueLeaders rank="5" />
+        <LeagueLeaders rank="6" />
+        <LeagueLeaders rank="7" />
+        <LeagueLeaders rank="8" />
+        <LeagueLeaders rank="9" />
+      </ScrollView>
     </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    //flex: 1,
-    //backgroundColor: "#fff",
-    //alignItems: "center",
-    //justifyContent: "center",
-  },
+  container: {},
 });

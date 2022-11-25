b14ce4938d10b4072032de528511868a63ae965a
Jesse1224 <jessekartabani@gmail.com>
2022-11-25 20:14:03 +1100

    Remove unused code

--- components/PlayerCareerStats.js
+++ components/PlayerCareerStats.js
@@ -1,29 +1,7 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 
 const PlayerCareerStats = () => {
-  {
-    /* TODO: Endpoint isnt working */
-  }
-  const [stats, setStats] = useState([]);
-  const getPlayerCareerStats = async () => {
-    try {
-      const response = await fetch(
-        "https://stats.nba.com/stats/playercareerstats?LeagueID=&PerMode=Totals&PlayerID=2544"
-      );
-      const json = await response.json();
-      const data = json;
-      console.log(data);
-      setStats(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getPlayerCareerStats();
-  }, []);
-
   return (
     <SafeAreaView>
       <Text>Test</Text>
